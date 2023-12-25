7bd30a301a12f89fa7541b86183605bc0b391da0
Lois <loisisar@outlook.com>
2023-12-25 02:00:50 +0000

    core: route mapping to methods, unique routes, builder pattern

--- src/demo/route.ts
+++ src/demo/route.ts
@@ -1,23 +1,38 @@
 import { type Server } from 'bun';
 import z from 'zod';
 
+// TODO: middleware for protected and public routes
+// DONE: define different routes but not using method names
+// DONE: map method to routes
+
 type Method<Input = z.Schema, Output = any> = (input: Input) => Output;
+
+class NPDevelopmentError extends Error {
+  constructor() {
+    super();
+  }
+}
 
 // 
 class NP {
-  router(procedures: Record<string, Method>) {
+  router(procedures: Record<string, Procedure>) {
     console.log({procedures});
     return (request: Request, server: Server) => {
       const url = new URL(request.url);
-      const route = procedures[url.pathname.slice(1)];
-      if (!route) return new Response("you fucked up it's normal", {
+      const procedure = procedures[url.pathname.slice(1)];
+      if (!procedure) return new Response("you fucked up it's normal", {
         status: 404,
       });
+
+      // if can't find return error "NPError: you haven't define the route for this method abc"
+      const handler = procedure._handler;
+      if (!handler) throw new NPDevelopmentError();
+
       const input = z.record(
         z.number(),
       );
-
-      const output = route(input);
+   
+      const output = handler(input);
 
       return new Response(JSON.stringify(output));
     };
@@ -27,12 +42,32 @@ class NP {
 const t = new NP();
 
 class Procedure {
-  endpoint(path: string) {
-    return this;
+  _path?: string;
+  _method?: string;
+  _handler?: (opts: any) => any;
+
+  constructor(opts: {
+    _path?: string;
+    _method?: string;
+    _handler?: (opts: any) => any;
+  } = {}) {
+    this._path = opts._path;
+    this._method = opts._method;
+    this._handler = opts._handler;
   }
 
-  method(_method: 'GET' | 'POST' | 'PUT' | 'DELETE') {
-    return this;
+  path(path: string) {
+    return new Procedure({
+      ...this,
+      _path: path,
+    });
+  }
+
+  method(method: 'GET' | 'POST' | 'PUT' | 'DELETE') {
+    return new Procedure({
+      ...this,
+      _method: method,
+    });
   }
 
   input(_schema: z.Schema) {
@@ -43,11 +78,27 @@ class Procedure {
     return this;
   }
 
-  query(method: (opts: any) => any) {
-    return method;
+  query(handler: (opts: any) => any) {
+     // TODO: Pretty error with color on console
+    if (this._method !== 'GET') throw new Error(`Queries do not support the "${this._method}" method, please use a mutation.`);
+    return new Procedure({
+      ...this,
+      // TODO: red alert, prevent query being called after a non "GET" method call
+      _method: 'GET',
+      _handler: handler,
+    });
   }
 
-  mutate(_method: (opts: any) => any) {}
+  mutate(handler: (opts: any) => any) {
+    // TODO: Pretty error with color on console
+    if (this._method === 'GET') throw new Error(`Mutations do not support the "GET" method, please use a query.`);
+    return new Procedure({
+      ...this,
+      // TODO: throw red squiggle at mutate if the method is "GET"
+      _method: this._method ?? "POST",
+      _handler: handler,
+    });
+  }
 }
 
 class TRPCError {
@@ -74,22 +125,24 @@ export const authorizedProcedure = publicProcedure
  
   // /app/hello
 export const appRouter = t.router({
+    // TODO: help users define their own headers
   getUsers: authorizedProcedure
-    .endpoint("/user/:id")
+    .path("/user/:id")
+    .method("GET")
     .query(() => {
       return {
         message: 'hello world',
       };
     }),
 
-  // deleteUser: authorizedProcedure
-  //   .endpoint("/user/:id")
-  //   .method('GET')
-  //   .mutate(() => {
-  //     return {
-  //       message: 'hello world',
-  //     };
-  //   }),
+  deleteUser: authorizedProcedure
+    .path("/asdsad/:id")
+    .method('DELETE')
+    .mutate(() => {
+      return {
+        message: 'hello world',
+      };
+    }),
 
   // createUser: authorizedProcedure
   //   .endpoint("/user/:id")
