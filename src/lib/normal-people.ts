d8ab8386568f92ca15c7fd53e0181f3f6204c674
luna <luna@wvvw.me>
2023-12-25 14:38:45 +1030

    feat: input

--- src/lib/normal-people.ts
+++ src/lib/normal-people.ts
@@ -6,7 +6,7 @@ import { HttpError } from './errors/http-error';
 
 export class NP {
   router(procedures: Record<string, Procedure>) {
-    return (request: Request, server: Server) => {
+    return async (request: Request, server: Server) => {
       const url = new URL(request.url);
 
       try {
@@ -24,14 +24,24 @@ export class NP {
         const handler = procedure._handler;
         if (!handler) throw new NPError(`You didn't define the handler for this route.`);
 
+        // Get the input data
+        const data =
+          request.method === 'GET'
+            ? Object.fromEntries(url.searchParams.entries())
+            : ((await request.json()) as Record<string, unknown>);
+
         // Validate input
-        const input = z.record(z.number());
+        const input = procedure._input ?? z.null();
+        const output = input.safeParse(data);
+
+        // Throw error if input fails validation
+        if (!output.success) throw new Error(output.error.message);
 
         // Generate output based on input
-        const output = handler(input);
+        const response = handler(input);
 
         // Convert response to a valid output for bun
-        return new Response(JSON.stringify(output));
+        return new Response(JSON.stringify(response));
       } catch (error: unknown) {
         // If a non-error is throw (e.g. a number) return a clean error instance
         if (!(error instanceof Error)) {
--- src/lib/procedure.ts
+++ src/lib/procedure.ts
@@ -3,20 +3,27 @@ import z from 'zod';
 export class Procedure {
   _path?: string;
   _method?: string;
+  _input?: z.Schema;
   _handler?: (opts: any) => any;
 
   constructor(
     opts: {
       _path?: string;
       _method?: string;
+      _input?: z.Schema;
       _handler?: (opts: any) => any;
     } = {},
   ) {
     this._path = opts._path;
     this._method = opts._method;
+    this._input = opts._input;
     this._handler = opts._handler;
   }
 
+  /**
+   * The path of the procedure (e.g. /user/:id)
+   * @param path
+   */
   path(path: string) {
     return new Procedure({
       ...this,
@@ -24,6 +31,11 @@ export class Procedure {
     });
   }
 
+  /**
+   * The HTTP method of the procedure (e.g. GET)
+   * @param method
+   * @returns
+   */
   method(method: 'GET' | 'POST' | 'PUT' | 'DELETE') {
     return new Procedure({
       ...this,
@@ -31,8 +43,16 @@ export class Procedure {
     });
   }
 
-  input(_schema: z.Schema) {
-    return this;
+  /**
+   * The input schema of the procedure (e.g. z.object({ name: z.string() }))
+   * @param _schema
+   * @returns
+   */
+  input(schema: z.Schema) {
+    return new Procedure({
+      ...this,
+      _input: schema,
+    });
   }
 
   use(_method: (opts: any) => void) {
@@ -41,8 +61,9 @@ export class Procedure {
 
   query(handler: (opts: any) => any) {
     // TODO: Pretty error with color on console
-    if (this._method !== 'GET')
+    if (this._method !== 'GET') {
       throw new Error(`Queries do not support the "${this._method}" method, please use a mutation.`);
+    }
     return new Procedure({
       ...this,
       // TODO: red alert, prevent query being called after a non "GET" method call
@@ -53,7 +74,9 @@ export class Procedure {
 
   mutate(handler: (opts: any) => any) {
     // TODO: Pretty error with color on console
-    if (this._method === 'GET') throw new Error(`Mutations do not support the "GET" method, please use a query.`);
+    if (this._method === 'GET') {
+      throw new Error(`Mutations do not support the "GET" method, please use a query.`);
+    }
     return new Procedure({
       ...this,
       // TODO: throw red squiggle at mutate if the method is "GET"
